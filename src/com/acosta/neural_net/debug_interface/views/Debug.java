e39711d0bf0802d57583bf59733b90839b7537e5
IanAcosta7 <ianacosta700@gmail.com>
2020-05-13 03:38:07 -0300

    Removed unnecessary loop in Debug.
    Signed-off-by: IanAcosta7 <ianacosta700@gmail.com>

--- src/com/acosta/neural_net/debug_interface/views/Debug.java
+++ src/com/acosta/neural_net/debug_interface/views/Debug.java
@@ -2,12 +2,9 @@ package com.acosta.neural_net.debug_interface.views;
 
 import com.acosta.neural_net.NeuralNet;
 import com.acosta.neural_net.debug_interface.INeuralNet;
-import com.acosta.neural_net.perceptron.Node;
 import com.acosta.neural_net.debug_interface.components.*;
-import com.acosta.neural_net.perceptron.Perceptron;
 
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class Debug extends Frame implements INeuralNet {
@@ -36,30 +33,25 @@ public class Debug extends Frame implements INeuralNet {
 
     // METHODS
     private void draw (Graphics g) {
-
         if (net != null) {
-            for (int i = 0; i < net.getLAYERS().size(); i++) {
-                for (int j = 0; j < net.getLAYERS().get(i).size(); j++) {
-                    g.setColor(Color.WHITE);
+            g.setColor(Color.WHITE);
 
-                    // DRAW A DATA COMPONENT
-                    NetData netData = new NetData(0, 0, 4, g);
-                    netData.setNeuralNet(net);
-                    //netData.setDrawBorders(true);
-                    netData.draw();
+            // DRAW A DATA COMPONENT
+            NetData netData = new NetData(0, 0, 4, g);
+            netData.setNeuralNet(net);
+            //netData.setDrawBorders(true);
+            netData.draw();
 
-                    // DRAW A SCHEME COMPONENT
-                    NetScheme netScheme = new NetScheme(netData.getWidth(), 0, 4, g);
-                    netScheme.setNeuralNet(net);
-                    //netScheme.setDrawBorders(true);
-                    netScheme.draw();
+            // DRAW A SCHEME COMPONENT
+            NetScheme netScheme = new NetScheme(netData.getWidth(), 0, 4, g);
+            netScheme.setNeuralNet(net);
+            //netScheme.setDrawBorders(true);
+            netScheme.draw();
 
-                    // DRAW AN ITERATION COMPONENT
-                    NetIteration netIteration = new NetIteration(netScheme.getX(), netScheme.getHeight() + 20, 4, g);
-                    netIteration.setNeuralNet(net);
-                    netIteration.draw();
-                }
-            }
+            // DRAW AN ITERATION COMPONENT
+            NetIteration netIteration = new NetIteration(netScheme.getX(), netScheme.getHeight() + 20, 4, g);
+            netIteration.setNeuralNet(net);
+            netIteration.draw();
         }
     }
 
